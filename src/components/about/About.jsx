c2051131b1e2da3f1fd3ba37da0499d1f0734b64
Yousra MSI <eljilaliyousra@gmail.com>
2024-01-05 18:19:03 +0100

    About - structure - done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
 import "./about.css"
+import Avatar from "../../assets/avatar.svg"
+import AboutBox from './AboutBox'
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className="about container section" id='about'>
+            <h2 className="section_title">A propos de moi</h2>
+
+            <div className="about_container grid">
+                <img src={Avatar} alt="" className="about_img" />
+
+                <div className="about_data grid">
+                    <div className="about_info">
+                        <p className="about_description">Je m'appelle Yousra El Jilali, Web Developer Junior Bruxelles, Belgique. Prête à relever les défis !</p>
+                        <a href="" className="btn">Télécharger mon CV</a>
+                    </div>
+
+                    <div className="about_skills grid">
+                        {/* HTML/CSS */}
+                        <div className="skills_data">
+                            <div className="skills_titles">
+                                <h3 className="skills_name">HTML/CSS</h3>
+                                <span className="skills_number">90%</span>
+                            </div>
+
+                            <div className="skills_bar">
+                                <span className="skills_percentage html_css"></span>
+                            </div>
+                        </div>
+
+
+                        {/* JAVASCRIPT */}
+
+                        <div className="skills_data">
+                            <div className="skills_titles">
+                                <h3 className="skills_name">JavaScript</h3>
+                                <span className="skills_number">80%</span>
+                            </div>
+
+                            <div className="skills_bar">
+                                <span className="skills_percentage javascript"></span>
+                            </div>
+                        </div>
+
+
+                        {/* PHP */}
+
+                        <div className="skills_data">
+                            <div className="skills_titles">
+                                <h3 className="skills_name">PHP</h3>
+                                <span className="skills_number">75%</span>
+                            </div>
+
+                            <div className="skills_bar">
+                                <span className="skills_percentage php"></span>
+                            </div>
+                        </div>
+
+
+                        {/* DESIGN */}
+
+                        <div className="skills_data">
+                            <div className="skills_titles">
+                                <h3 className="skills_name">DESIGN</h3>
+                                <span className="skills_number">60%</span>
+                            </div>
+
+                            <div className="skills_bar">
+                                <span className="skills_percentage design"></span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <AboutBox />
+        </section>
     )
 }
 
---
+++ src/components/about/AboutBox.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+// TODO Softskills
+
+function AboutBox() {
+    return (
+        <div className="about_boxes grid">
+
+            <div className="about_box">
+                <i className="about_icon icon-fire"></i>
+
+                <div>
+                    <h3 className="about_title">198</h3>
+                    <span className="about_subtitle">Project completed</span>
+                </div>
+            </div>
+
+            <div className="about_box">
+                <i className="about_icon icon-cup"></i>
+
+                <div>
+                    <h3 className="about_title">5670</h3>
+                    <span className="about_subtitle">Cup of coffee</span>
+                </div>
+            </div>
+
+            <div className="about_box">
+                <i className="about_icon icon-people"></i>
+
+                <div>
+                    <h3 className="about_title">427</h3>
+                    <span className="about_subtitle">Satisfied clients</span>
+                </div>
+            </div>
+
+            <div className="about_box">
+                <i className="about_icon icon-badge"></i>
+
+                <div>
+                    <h3 className="about_title">35</h3>
+                    <span className="about_subtitle">Nominees winner</span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutBox
