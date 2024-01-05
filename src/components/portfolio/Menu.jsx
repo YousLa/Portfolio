5c3cb9cd3d203adddf5daac6207f7807334867fc
Yousra MSI <eljilaliyousra@gmail.com>
2024-01-05 19:37:38 +0100

    Projets - commencé

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Todo from "../../assets/todo.jpg"
+
+const Menu = [
+    {
+        id: 1,
+        image: Todo,
+        title: "Plateforme de streaming",
+        category: "PHP"
+
+    },
+    {
+        id: 2,
+        // image: CodeCanvas,
+        image: Todo,
+        title: "Démystificateur de langages de programmation",
+        category: "PHP"
+
+    },
+    {
+        id: 3,
+        // image: Agregator,
+        image: Todo,
+        title: "Agrégateur de contenu",
+        category: "NodeJs"
+
+    },
+    {
+        id: 4,
+        // image: FlowYourLife,
+        image: Todo,
+        title: "Portfolio",
+        category: "WordPress"
+
+    },
+    {
+        id: 5,
+        // image: Pokedex,
+        image: Todo,
+        title: "Pokedex",
+        category: "React"
+
+    },
+    {
+        id: 6,
+        // image: AdressBook,
+        image: Todo,
+        title: "Adress Book",
+        category: "PHP"
+
+    }
+]
+
+export default Menu
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./portfolio.css"
+import Menu from './Menu'
 
 const Portfolio = () => {
+    const [items, setItems] = useState(Menu);
     return (
-        <div>Portfolio</div>
+        <section className="work container section" id="work">
+            <h2 className="section_title">Projets</h2>
+
+            <div className="work_filters">
+                <span className="work_item">Tout</span>
+                <span className="work_item">JavaScript</span>
+                <span className="work_item">PHP</span>
+                <span className="work_item">WordPress</span>
+                <span className="work_item">React</span>
+                <span className="work_item">NodeJS</span>
+            </div>
+
+            <div className="work_container grid">
+                {items.map((elem) => {
+                    const { id, image, title, category } = elem;
+                    return (
+                        <div className="work_car" key={id}>
+                            <div className="work_thumbnail">
+                                <img src={image} alt="" className="work_img" />
+                                <div className="work_mask"></div>
+                            </div>
+
+                            <span className="work_category">{category}</span>
+                            <h3 className="work_title">{title}</h3>
+                            <a href="" className="work_button">
+                                <i className="icon-link work_button-icon"></i>
+                            </a>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
     )
 }
 
