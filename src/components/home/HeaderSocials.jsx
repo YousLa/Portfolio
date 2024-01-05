c302ba5e134241ca44b5a95d5bb881c9ec5b195b
Yousra MSI <eljilaliyousra@gmail.com>
2024-01-05 12:03:36 +0100

    Home - half done

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Linkedin from "../../assets/linkedin.svg"
+import Github from "../../assets/github.svg"
+// import Resume from "../../assets/resume.svg"
+import Gmail from "../../assets/gmail.svg"
+
+const HeaderSocials = () => {
+    return (
+        <div className="home_socials">
+            <a href="" className="home_social-link" target="_blank">
+                {/* <i class="fa-brands fa-linkedin-in"></i>             */}
+                <img src={Linkedin} alt="LinkedIn" />
+            </a>
+
+            <a href="" className="home_social-link" target="_blank">
+                <i class="fa-brands fa-github"></i>
+                <img src={Github} alt="Github" />
+            </a>
+
+            <a href="" className="home_social-link" target="_blank">
+                <i class="fa-regular fa-file-pdf"></i>
+                {/* <img src={Resume} alt="Curriculum Vitae" /> */}
+            </a>
+
+            <a href="" className="home_social-link" target="_blank">
+                <i class="fa-solid fa-at"></i>
+                <img src={Gmail} alt="Gmail" />
+            </a>
+        </div >
+    )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import "./home.css"
+import React from 'react';
+import "./home.css";
+import Avatar from "../../assets/avatar.svg";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="home" id="home">
+            <div className="intro">
+                <img src={Avatar} alt="" className='home_img' />
+                <h1 className="home_name">Yousra El Jilali</h1>
+                <span className="home_education">Je suis Web Developer Junior</span>
+
+                <HeaderSocials />
+
+                <a href="#contact" className="btn">Engagez-moi</a>
+
+                <ScrollDown />
+            </div>
+        </section>
     )
 }
 
