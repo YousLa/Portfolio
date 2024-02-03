fa60324e91106dbc0f94b011ae0e5f88f7dd2b4e
Yousra MSI <eljilaliyousra@gmail.com>
2024-02-03 20:26:12 +0100

    Home | English

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,13 +9,14 @@ const Home = () => {
     return (
         <section className="home" id="home">
             <div className="intro">
+                {/* TODO Changer l'image */}
                 <img src={Avatar} alt="" className='home_img' />
                 <h1 className="home_name">Yousra El Jilali</h1>
-                <span className="home_education">Je suis Web Developer Junior</span>
+                <span className="home_education">I am a Web Developer Junior</span>
 
                 <HeaderSocials />
 
-                <a href="#contact" className="btn">Contactez-moi</a>
+                <a href="#contact" className="btn">Contact me</a>
 
                 <ScrollDown />
             </div>
