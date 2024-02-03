3d9425a4982b55d697a2e5e25a76eb229fbbb21b
Yousra MSI <eljilaliyousra@gmail.com>
2024-02-03 23:14:02 +0100

    About | Changement total + Css + Responsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,83 +1,81 @@
 import React from 'react'
 import "./about.css"
-import Avatar from "../../assets/svg/avatar.svg"
-import AboutBox from './AboutBox'
 import CV from '../../assets/doc/CV-Yousra-El-Jilali.pdf'
 
 const About = () => {
     return (
         <section className="about container section" id='about'>
-            <h2 className="section_title">A propos de moi</h2>
+            <h2 className="section_title">About me</h2>
 
-            <div className="about_container grid">
-                <img src={Avatar} alt="" className="about_img" />
+            <div className="about_container">
+
 
                 <div className="about_data grid">
                     <div className="about_info">
-                        <p className="about_description">Je m'appelle Yousra El Jilali, Web Developer Junior Bruxelles, Belgique. Prête à relever les défis !</p>
-                        <a href={CV} className="btn" target="_blank">Télécharger mon CV</a>
+                        <p className="about_description">Hey there! I'm Yousra El Jilali, a 27-year-old adventurer based in the vibrant city of Brussels, Belgium. I've always been drawn to the endless possibilities that technology offers, which led me to dive headfirst into the world of development.</p>
+                        <p className="about_description">By day, you'll find me honing my skills in front-end development at Interface3, where I'm learning to craft beautiful and user-friendly interfaces that bring ideas to life. At night, I'm immersed in my first year of Computer Science studies at Ephec, where I'm eagerly absorbing knowledge like a sponge.</p>
+
                     </div>
 
-                    <div className="about_skills grid">
-                        {/* HTML/CSS */}
-                        <div className="skills_data">
-                            <div className="skills_titles">
-                                <h3 className="skills_name">HTML/CSS</h3>
-                                <span className="skills_number">90%</span>
-                            </div>
-
-                            <div className="skills_bar">
-                                <span className="skills_percentage html_css"></span>
-                            </div>
+                    <div className="about_skills">
+                        <div className='intro_skills'>
+                            <p className="about_description">My journey in the world of technology has been a thrilling ride so far, filled with challenges, triumphs, and countless lines of code. Along the way, I've dabbled in various technologies such as :</p>
                         </div>
+                        <div className='icons_skills'>
+
+                            <i class="devicon-html5-plain colored"></i>
+
+                            <i class="devicon-css3-plain colored"></i>
+
+                            <i class="devicon-sass-original colored"></i>
+
+                            <i class="devicon-bootstrap-plain colored"></i>
 
 
-                        {/* JAVASCRIPT */}
+                            <i class="devicon-tailwindcss-plain colored"></i>
 
-                        <div className="skills_data">
-                            <div className="skills_titles">
-                                <h3 className="skills_name">JavaScript</h3>
-                                <span className="skills_number">80%</span>
-                            </div>
+                            <i class="devicon-javascript-plain colored"></i>
 
-                            <div className="skills_bar">
-                                <span className="skills_percentage javascript"></span>
-                            </div>
+                            <i class="devicon-mysql-plain colored"></i>
+
+                            <i class="devicon-php-plain colored"></i>
+
+                            <i class="devicon-wordpress-plain colored"></i>
+
+                            <i class="devicon-nodejs-plain colored"></i>
+
+                            <i class="devicon-express-original colored"></i>
+
+                            <i class="devicon-mongodb-plain colored"></i>
+
+                            <i class="devicon-react-original colored"></i>
+
+                            <i class="devicon-java-plain colored"></i>
+
+                            <i class="devicon-csharp-plain colored"></i>
+
+                            <i class="devicon-figma-plain colored"></i>
+
+                            <i class="devicon-canva-original colored"></i>
+
+                            <i class="devicon-illustrator-line colored"></i>
+
+                            <i class="devicon-photoshop-line colored"></i>
+
+                            <i class="devicon-xd-line colored"></i>
+
+                            <i class="devicon-git-plain colored"></i>
+
+                            <i class="devicon-github-original colored"></i>
+
+
                         </div>
-
-
-                        {/* PHP */}
-
-                        <div className="skills_data">
-                            <div className="skills_titles">
-                                <h3 className="skills_name">PHP</h3>
-                                <span className="skills_number">75%</span>
-                            </div>
-
-                            <div className="skills_bar">
-                                <span className="skills_percentage php"></span>
-                            </div>
-                        </div>
-
-
-                        {/* DESIGN */}
-
-                        <div className="skills_data">
-                            <div className="skills_titles">
-                                <h3 className="skills_name">DESIGN</h3>
-                                <span className="skills_number">60%</span>
-                            </div>
-
-                            <div className="skills_bar">
-                                <span className="skills_percentage design"></span>
-                            </div>
-                        </div>
-
+                    </div>
+                    <div className='container_download-resume'>
+                        <a href={CV} className="btn" target="_blank">Download my resume</a>
                     </div>
                 </div>
             </div>
-
-            <AboutBox />
         </section>
     )
 }
