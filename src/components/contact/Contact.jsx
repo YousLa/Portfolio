a2eaaa104190fbb2d590c94606960df55a1e3719
Yousra MSI <eljilaliyousra@gmail.com>
2024-02-04 15:43:12 +0100

    Contact | css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,11 +7,12 @@ const Contact = () => {
         <section className="contact container section" id="contact">
             <h2 className="section_title">Contact</h2>
 
-            <div className="contact_container grid">
+            <div className="contact_container">
                 <div className="contact_info">
-                    <h3 className="contact_title">Let's talk about everything !</h3>
+                    <h3 className="contact_title">Let's talk !</h3>
 
-                    <p className="contact_details">Don't like forms ? Send an email. 👌</p>
+                    <p className="contact_details"> Feeling adventurous ? Dive into my LinkedIn, explore my gitHub, or simply drop me an email. Let's make this networking thin fun !</p>
+                    <p className="contact_details">Want to know more ? Download my resume.</p>
 
                     <FooterSocials />
                 </div>
