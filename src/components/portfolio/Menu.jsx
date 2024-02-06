149e6be2b8c0631b6835db89d9782eb583356bd8
Yousra MSI <eljilaliyousra@gmail.com>
2024-02-06 16:28:41 +0100

    Video Portfolio

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -6,11 +6,17 @@ import Agregator from "../../assets/image/projects/Agregator.png"
 import FlowYourLife from "../../assets/image/projects/FlowYourLife.png"
 import AdressBook from "../../assets/image/projects/AdressBook.png"
 import Pokedex from "../../assets/image/projects/Pokedex.png"
+import KimiaVid from "../../assets/video/Kimia.mp4"
+import AgregatorVid from "../../assets/video/Agregator.mp4"
+import NewsletterVid from "../../assets/video/Newsletter.mp4"
+import CodeCanvaVid from "../../assets/video/CodeCanva.mp4"
+import AdressBookVid from "../../assets/video/AdressBook.mp4"
 
 const Menu = [
     {
         id: 1,
         image: Kimia,
+        video: KimiaVid,
         // title: "Plateforme de streaming",
         title: "Kimia",
         category: "PHP",
@@ -20,6 +26,7 @@ const Menu = [
     {
         id: 2,
         image: CodeCanvas,
+        video: CodeCanvaVid,
         // title: "Démystificateur de langages de programmation",
         title: "Code Canvas",
         category: "JavaScript",
@@ -28,6 +35,7 @@ const Menu = [
     {
         id: 3,
         image: Agregator,
+        video: AgregatorVid,
         // title: "Agrégateur de contenu",
         title: "Agregator",
         category: "NodeJS",
@@ -37,6 +45,7 @@ const Menu = [
     {
         id: 4,
         image: FlowYourLife,
+        video: null,
         // title: "Career Coach",
         title: "Flow Your Life",
         category: "WordPress",
@@ -46,6 +55,7 @@ const Menu = [
     {
         id: 5,
         image: CoverLetter,
+        video: NewsletterVid,
         title: "Newsletter",
         category: "HTML",
         link: "https://github.com/YousLa/Cover-Letter"
@@ -54,6 +64,7 @@ const Menu = [
     {
         id: 6,
         image: AdressBook,
+        video: AdressBookVid,
         title: "Adress Book",
         category: "PHP",
         link: "https://github.com/YousLa/Address-Book"
@@ -61,6 +72,7 @@ const Menu = [
     {
         id: 7,
         image: Pokedex,
+        video: null,
         title: "Pokedex",
         category: "React",
         link: ""
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react'
-import "./portfolio.css"
-import Menu from './Menu'
+import React, { useState } from 'react';
+import "./portfolio.css";
+import Menu from './Menu';
+import ReactModal from 'react-modal';
 
 const Portfolio = () => {
     const [items, setItems] = useState(Menu);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+
     const filterItem = (categoryItem) => {
         const updatedItems = Menu.filter((curElem) => {
             return curElem.category === categoryItem;
-        })
+        });
 
         setItems(updatedItems);
     }
+
+    const handleOpenModal = (item) => {
+        setSelectedItem(item);
+        setShowModal(true);
+    }
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    }
+
     return (
         <section className="work container section" id="project">
             <h2 className="section_title">Projects</h2>
@@ -27,25 +41,47 @@ const Portfolio = () => {
 
             <div className="work_container grid">
                 {items.map((elem) => {
-                    const { id, image, title, category, link } = elem;
+                    const { id, image, video, title, category, link } = elem;
                     return (
                         <div className="work_card" key={id}>
                             <div className="work_thumbnail">
-                                <img src={image} alt="" className="work_img" />
+                                <img src={image} alt={title} className="work_img" />
                                 <div className="work_mask"></div>
                             </div>
 
                             <span className="work_category">{category}</span>
                             <h3 className="work_title">{title}</h3>
-                            <a href={link} target="_blank" className="work_button">
-                                <i className="icon-link work_button-icon"></i>
-                            </a>
+                            <button className="work_button" onClick={() => handleOpenModal(elem)}>
+                                <i className="fa-solid fa-plus"></i>
+                            </button>
                         </div>
                     )
                 })}
             </div>
+            {/* TODO : Scroll back block | Card */}
+            <ReactModal
+                isOpen={showModal}
+                onRequestClose={handleCloseModal}
+                contentLabel="Modal Content"
+            >
+                <div>
+
+                    {selectedItem && (
+                        <>
+                            {selectedItem.video ? (
+                                <video className='modal-media' src={selectedItem.video} autoPlay loop></video>
+                            ) : (
+                                <img className='modal-media' src={selectedItem.image} alt={selectedItem.title} />
+                            )}
+                        </>
+                    )}
+                </div>
+                <button onClick={handleCloseModal}>
+                    <i className="fa-solid fa-xmark"></i>
+                </button>
+            </ReactModal>
         </section>
     )
 }
 
-export default Portfolio
+export default Portfolio;
